644e69ddf5ac9feda3e2b80c6120fa0cd65da1a4
ValentinaGrin <vbhygiene@gmail.com>
2021-10-03 17:05:31 -0500

    Added new package and new java file

---
+++ src/package_practice_two/SecondJavaPractice.java
@@ -0,0 +1,14 @@
+package package_practice_two;
+
+public class SecondJavaPractice {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5 ; i++) {
+            System.out.println("Hello Universe");
+
+
+        }
+        
+    }
+}
